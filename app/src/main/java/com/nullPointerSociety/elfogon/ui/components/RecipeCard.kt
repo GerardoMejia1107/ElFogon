e195189b83dc21d99149018df99b870468b2c118
Miguel Mejia <00104923@uca.edu.sv>
2025-06-04 14:24:10 -0600

    :bug: Arreglando import de Coil

--- app/src/main/java/com/nullPointerSociety/elfogon/ui/components/RecipeCard.kt
+++ app/src/main/java/com/nullPointerSociety/elfogon/ui/components/RecipeCard.kt
@@ -16,7 +16,8 @@ import androidx.compose.ui.unit.dp
 import androidx.compose.ui.text.style.TextAlign
 
 import androidx.compose.ui.unit.sp
-import coil3.compose.rememberAsyncImagePainter
+import coil.compose.rememberAsyncImagePainter
+
 import com.nullPointerSociety.elfogon.model.Recipe
 
 @Composable
