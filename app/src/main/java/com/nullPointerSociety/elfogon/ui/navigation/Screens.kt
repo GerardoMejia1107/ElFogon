45439d02adc785a76fce4368754b28d410f9b46d
Miguel Mejia <00104923@uca.edu.sv>
2025-06-04 15:00:31 -0600

    :art: Agregando objetos representantes de las rutas de navegacion son Serializable

---
+++ app/src/main/java/com/nullPointerSociety/elfogon/ui/navigation/Screens.kt
@@ -0,0 +1,21 @@
+package com.nullPointerSociety.elfogon.ui.navigation
+
+import kotlinx.serialization.Serializable
+
+@Serializable
+object HomeScreenNav
+
+@Serializable
+object KeptRecipesScreenNav
+
+@Serializable
+object UserProfileScreenNav
+
+@Serializable
+object MadeRecipesScreenNav
+
+@Serializable
+object RecipeDetailsScreenNav
+
+@Serializable
+object SearchByFilterScreenNav
