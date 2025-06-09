c6f716d0a0ff7109127447eda5b91d9e5a6019d8
Miguel Mejia <00104923@uca.edu.sv>
2025-06-08 21:00:24 -0600

    :building_construction: Se arreglaron las estructuras de las pantallas

--- app/src/main/java/com/nullPointerSociety/elfogon/ui/screens/SavedRecipesScreen.kt
+++ app/src/main/java/com/nullPointerSociety/elfogon/ui/screens/SavedRecipesScreen.kt
@@ -10,16 +10,11 @@ import com.nullPointerSociety.elfogon.ui.layout.CustomTopBar
 
 @Composable
 fun SavedRecipesScreen() {
-    Scaffold(
-        topBar = {
-            CustomTopBar("Mis Recetas Guardadas")
-        }
-    ) { innerPadding ->
-        Column(modifier = Modifier.padding(innerPadding)) {
+
+        Column() {
             Text(
                 "Aquí se mostrarán las recetas que has guardado.",
             )
         }
 
     }
-}
