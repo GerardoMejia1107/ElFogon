4ec5919fad3a3804774587c290628d2f3c407051
GerardoMejia1107 <00104923@uca.edu.sv>
2025-06-27 10:50:23 -0600

    :memo: Commir de prueba por cambio de SO (verificacion de firma)

---
+++ app/src/main/java/com/nullPointerSociety/elfogon/data/repository/impl/SystemRepositoryImp.kt
@@ -0,0 +1,6 @@
+package com.nullPointerSociety.elfogon.data.repository.impl
+
+import com.nullPointerSociety.elfogon.data.repository.SystemRepository
+
+class SystemRepositoryImp() : SystemRepository {
+}
