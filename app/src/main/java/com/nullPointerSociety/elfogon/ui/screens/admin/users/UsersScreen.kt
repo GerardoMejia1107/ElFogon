aab352e4c5ba166151c02d3338607c7d409b8ad0
GerardoMejia1107 <00104923@uca.edu.sv>
2025-07-03 02:00:22 -0600

    :construction: Se agrego el viewmodel y la configuración incial para la pantalla de UsersScreen (admin)

--- app/src/main/java/com/nullPointerSociety/elfogon/ui/screens/admin/users/UsersScreen.kt
+++ app/src/main/java/com/nullPointerSociety/elfogon/ui/screens/admin/users/UsersScreen.kt
@@ -1,2 +1,36 @@
 package com.nullPointerSociety.elfogon.ui.screens.admin.users
 
+import androidx.compose.foundation.layout.Arrangement
+import androidx.compose.foundation.layout.PaddingValues
+import androidx.compose.foundation.layout.fillMaxSize
+import androidx.compose.foundation.layout.padding
+import androidx.compose.foundation.lazy.LazyColumn
+import androidx.compose.foundation.lazy.LazyListState
+import androidx.compose.material3.Text
+import androidx.compose.runtime.Composable
+import androidx.compose.ui.Alignment
+import androidx.compose.ui.Modifier
+import androidx.compose.ui.unit.dp
+
+@Composable
+fun UsersScreen(
+    modifier: Modifier = Modifier,
+    scrollState: LazyListState = LazyListState(0, 0),
+) {
+    LazyColumn(
+        state = scrollState,
+        modifier = modifier
+            .fillMaxSize()
+            .padding(top = 20.dp),
+        contentPadding = PaddingValues(bottom = 125.dp),
+        horizontalAlignment = Alignment.CenterHorizontally,
+        verticalArrangement = Arrangement.Center
+    ) {
+        item {
+            Text("Aquí se mostrarán los usuarios registrados en la aplicación.")
+        }
+
+
+    }
+
+}
--- app/src/main/java/com/nullPointerSociety/elfogon/ui/screens/admin/users/UsersViewModel.kt
+++ app/src/main/java/com/nullPointerSociety/elfogon/ui/screens/admin/users/UsersViewModel.kt
@@ -1,4 +1,36 @@
 package com.nullPointerSociety.elfogon.ui.screens.admin.users
 
-class UsersViewModel {
+import android.util.Log
+import androidx.lifecycle.ViewModel
+import androidx.lifecycle.ViewModelProvider
+import androidx.lifecycle.ViewModelProvider.AndroidViewModelFactory.Companion.APPLICATION_KEY
+import androidx.lifecycle.viewModelScope
+import androidx.lifecycle.viewmodel.initializer
+import androidx.lifecycle.viewmodel.viewModelFactory
+import com.nullPointerSociety.elfogon.DelFogonApplication
+import com.nullPointerSociety.elfogon.data.repository.AdminRepository
+import com.nullPointerSociety.elfogon.ui.screens.admin.dashboard.DashboardViewModel
+import kotlinx.coroutines.launch
+
+class UsersViewModel(
+    private val adminRepository: AdminRepository
+) : ViewModel() {
+    val allUsers = adminRepository.allUsers
+
+    init {
+        viewModelScope.launch {
+            adminRepository.fetchAllUsers()
+        }
+    }
+
+    companion object {
+        val Factory: ViewModelProvider.Factory = viewModelFactory {
+            initializer {
+                val app = this[APPLICATION_KEY] as DelFogonApplication
+                DashboardViewModel(
+                    app.appProvider.provideAdminRepository()
+                )
+            }
+        }
+    }
 }
