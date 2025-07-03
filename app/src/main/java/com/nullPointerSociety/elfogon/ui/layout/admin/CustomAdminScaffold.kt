a563873c3fdfd4d8083793a82cf493563c279758
GerardoMejia1107 <00104923@uca.edu.sv>
2025-07-03 00:52:42 -0600

    :art: Se agrego el diseño inicial del CustomAdminScaffold para el administrador

---
+++ app/src/main/java/com/nullPointerSociety/elfogon/ui/layout/admin/CustomAdminScaffold.kt
@@ -0,0 +1,39 @@
+package com.nullPointerSociety.elfogon.ui.layout.admin
+
+import androidx.compose.material3.Scaffold
+import androidx.compose.runtime.Composable
+import androidx.compose.runtime.getValue
+import androidx.compose.runtime.mutableStateOf
+import androidx.compose.runtime.saveable.rememberSaveable
+import androidx.compose.ui.platform.LocalContext
+import androidx.navigation.compose.currentBackStackEntryAsState
+import androidx.navigation.compose.rememberNavController
+
+@Composable
+fun CustomAdminScaffold(
+
+) {
+    val navController = rememberNavController()
+    var titleScreen = rememberSaveable { mutableStateOf("") }
+    val navBackStackEntry by navController.currentBackStackEntryAsState()
+    val currentRoute = navBackStackEntry?.destination?.route
+    val previousRoute = navController.previousBackStackEntry?.destination?.route
+
+    fun onItemSelected(route: Any) {
+        navController.navigate(route)
+    }
+
+    val context = LocalContext.current
+
+
+    Scaffold(
+        topBar = { CustomAdminTopBar() },
+        bottomBar = {
+            CustomAdminButtonBar(selectedRoute = currentRoute, onItemSelected = { routeObject ->
+                onItemSelected(routeObject)
+            })
+        },
+    ) { innerPadding -> }
+
+
+}
